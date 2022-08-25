71c27e4737edfd6f228eae30bf081975b1ea1efb
Luiz Claudio <azevedolcc@gmail.com>
2022-08-24 22:41:41 -0300

    9.18. Classes CSS dinâmicas com ngClass

--- src/app/funcionario-card/funcionario-card.component.ts
+++ src/app/funcionario-card/funcionario-card.component.ts
@@ -3,7 +3,13 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-funcionario-card',
   templateUrl: './funcionario-card.component.html',
-  styleUrls: ['./funcionario-card.component.css']
+  //styleUrls: ['./funcionario-card.component.css']
+  styles: [`
+    .card-body {
+      text-transform: uppercase;
+      color: blue;
+    }
+  `]
 })
 export class FuncionarioCardComponent {
 // o @Input é utilizado para a definção de uma variavel que será utilizada
@@ -13,5 +19,22 @@ export class FuncionarioCardComponent {
 // Podemos utilizar o Input com outro nome que não seja o funcionario
 // nesse caso no app.component não vamos utilizar o nome funcionario para a variavel.
 @Input('obj') funcionario: any;
+
+//getClassesCss() {
+ // return ['badge', 'badge-default'];
+//}
+isAdmin() {
+  return this.funcionario.nome.startsWith('T');
+}
+getEstilosCartao() {
+  return {
+    // a borda dos cartoes vão aumentando
+ //   'border-width': this.funcionario.id + 'px',
+    'border-width.px': this.funcionario.id,
+    // os cartão vão mudando de cor
+    backgroundColor: this.funcionario.id % 2 === 0 
+                  ? 'lightblue'  : 'lightgreen'
+  };
+}
  
 }
